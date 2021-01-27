4e453c969d4a8476696dc313984e35d5f5a0f674
Sheldon Anthony <shakes082@gmail.com>
2021-01-27 15:10:07 +0200

    Increased Test Coverage...

--- src/test/java/za/co/mamamoney/ussdservice/service/USSDMenu2ProcessorTest.java
+++ src/test/java/za/co/mamamoney/ussdservice/service/USSDMenu2ProcessorTest.java
@@ -3,6 +3,8 @@ package za.co.mamamoney.ussdservice.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,25 +12,29 @@ import za.co.mamamoney.ussdservice.dtos.USSDRequest;
 import za.co.mamamoney.ussdservice.dtos.USSDResponse;
 
 @SpringBootTest
-public class USSDMenu1ProcessorTest {
-	
+public class USSDMenu2ProcessorTest {
+
 	@Autowired
-	private USSDMenu1Processor uSSDMenu1Processor;
+	private USSDMenu2Processor uSSDMenu2Processor;
 	
 	@Test
 	public void contextLoads() throws Exception {
-		assertThat(uSSDMenu1Processor).isNotNull();
+		assertThat(uSSDMenu2Processor).isNotNull();
 	}
 	
 	@Test
 	public void testExecute01() throws Exception{
+		Map<String, String> requestOptions = new HashMap<String, String>();
+		requestOptions.put("countryCode", "01");
+		requestOptions.put("amount", "100.00");
+		
 		USSDRequest ussdRequest = new USSDRequest();
 		ussdRequest.setMsisdn("27829611111");
-		ussdRequest.setUserEntry(1);
-		ussdRequest.setRequestOptions(new HashMap<String, String>());
+		ussdRequest.setUserEntry(2);
+		ussdRequest.setRequestOptions(requestOptions);
 		
-		USSDResponse ussdResponse = uSSDMenu1Processor.processRequest(ussdRequest);
-		assertEquals(2, ussdResponse.getResponseData().size());
+		USSDResponse ussdResponse = uSSDMenu2Processor.processRequest(ussdRequest);
+		assertEquals(1, ussdResponse.getResponseData().size());
 	}
 
 }
